b1fd06ce51dc08fac3b8e73e067a33c6d82a21b3
= <snangles.void@gmail.com>
2019-12-24 16:28:06 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const sg = require("@sendgrid/mail");
+
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("email server working");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const html = `
+    <h3>Firstname: ${req.body.firstname}</h3>
+    <h3>Lastname: ${req.body.lastname}</h3>
+    <p>Email: ${req.body.email}</p>
+    <p>${req.body.message}</p>
+  `;
+
+  const msg = {
+    to: "janikhotz@gmail.com",
+    from: req.body.email,
+    subject: "Neue Nachricht von der Webseite",
+    text: req.body.message,
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+  };
+  sg.send(msg);
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("listening on port " + process.env.PORT);
+});
